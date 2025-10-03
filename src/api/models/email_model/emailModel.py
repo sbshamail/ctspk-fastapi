7338a9b15a16ea30957d7443027fe62ff0fad6ca
ghalibraza <rurazza@gmail.com>
2025-10-03 10:05:14 +0500

    add field in html_content in email_template and change content field text to jsonb

--- src/api/models/email_model/emailModel.py
+++ src/api/models/email_model/emailModel.py
@@ -2,6 +2,7 @@ from typing import Literal, Optional
 from sqlalchemy import Column, Enum,Index
 from datetime import datetime
 from sqlmodel import SQLModel, Field
+from sqlalchemy.dialects.postgresql import JSONB
 from src.api.models.baseModel import TimeStampedModel, TimeStampReadModel
 from enum import Enum as PyEnum
 
@@ -19,7 +20,8 @@ class Emailtemplate(TimeStampedModel, table=True):
     name: str = Field(max_length=191, nullable=False)
     slug: str = Field(max_length=191, index=True, unique=True)  # ✅ indexed + unique
     subject: str = Field(max_length=300, nullable=False)
-    content: Optional[str] = None
+    content:  Optional[dict] = Field(default=None, sa_column=Column(JSONB))
+    html_content: Optional[str] = None
     is_active: bool = Field(default=True)
     language: str = Field(default="en", max_length=191)
     
@@ -31,14 +33,16 @@ class Emailtemplate(TimeStampedModel, table=True):
 class EmailCreate(SQLModel):
     name: str
     subject: Optional[str] = None
-    content: Optional[str] = None
+    content: Optional[dict] = None
+    html_content: Optional[str] = None
     is_active: bool = True
 
 class EmailRead(TimeStampReadModel):
     id:int
     name: str
     subject: Optional[str]
-    content: Optional[str]
+    content: Optional[dict] = None
+    html_content: Optional[str] = None
     is_active: bool 
 
 class EmailActivate(SQLModel):
@@ -47,5 +51,6 @@ class EmailActivate(SQLModel):
 class EmailUpdate(SQLModel):
     name: str
     subject: Optional[str] = None
-    content: Optional[str] = None
+    content: Optional[dict] = None
+    html_content: Optional[str] = None
     is_active: bool = True
